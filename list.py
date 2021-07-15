1743f6ca8d4cdd0e6fc10f44e75d8f0d3467f745
Krishnapandi <twinkrishna7@gmail.com>
2021-07-16 00:44:32 +0530

    list.py added 15-07-21

---
+++ list.py
@@ -0,0 +1,39 @@
+thislist = ["yuvan sankar raja", 7, ("krishna", "varun"), {"krishna", "varun"}]
+# for x in thislist:
+#     print(x)
+# a1 = thislist[0]
+# a2 = thislist[1]
+# a3 = thislist[2]
+# a4 = thislist[3]
+# print(type(a1), type(a2), type(a3), type(a4))
+# #! list are mutable here we add delete modify the original values
+# thislist.append("krishna")
+# print(thislist)
+#! create list using constructor method
+#! and also convert tuples and set to list
+# thislist = list(("krishna", "kavin", 3, True))
+# print(thislist)
+# print(type(thislist))
+# #! access list
+# print(thislist[0])
+# print(thislist[-1])
+# #! slice string (some range of list index)
+# print(thislist[0:1])
+# print(thislist[-2:-1])
+# #! check value present or not
+# if "krishna" in thislist:
+#     print("yes 'krishna' present in the list")
+# ! change values in the list by index
+# thislist[0] = "krishnapandi"
+# print(thislist)
+# # ! change values in the list by range of index
+# thislist[1:2] = ["karthick", "karupasamy"]
+# print(thislist)
+# #! insert a new value without remove any of existing values
+# thislist.insert(6, "kathirvelan")
+# print(thislist)
+#! insert a new value at end of the list without remove any of existing values
+#! in this method we add one items at the time 
+thislist.append("cherry")
+print(thislist)
+#! to add more then one 
