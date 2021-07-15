7bb639be46430c8388992021d7814880fd4be4b7
Krishnapandi <twinkrishna7@gmail.com>
2021-07-15 11:47:52 +0530

    array.js updated on 15-07-2021

--- array.js
+++ array.js
@@ -1,12 +1,12 @@
-let users = ["shashi", "krishna", "bhat", "ragu"];
-let U1 = users[0];
-console.log(users[0]);
-let U2 = users[1];
-console.log(users[1]);
-let U3 = users[2];
-console.log(users[2]);
-let U4 = users[3];
-console.log(users[3]);
+// let users = ["shashi", "krishna", "bhat", "ragu"];
+// let U1 = users[0];
+// console.log(users[0]);
+// let U2 = users[1];
+// console.log(users[1]);
+// let U3 = users[2];
+// console.log(users[2]);
+// let U4 = users[3];
+// console.log(users[3]);
 
 // console.log(U1, U2, U3, U4);
 
@@ -53,22 +53,88 @@ console.log(users[3]);
 
 //!===============LOOP over an array===============
 
-users[4] = "vivek";
+// users[4] = "vivek";
 // console.log(users.length);
 
 // for (let i = 0; i <= users.length - 1; i++) {
 //   console.log(users[i]);
 // }
 //! =============== Array or Not ===============
-let x = ["krishna"];
-console.log(typeof x); // object
-console.log(Array.isArray(x)); //boolean value
+// let x = ["krishna"];
+// console.log(typeof x); // object
+// console.log(Array.isArray(x)); //boolean value
 
 // example
 
-if (Array.isArray(x) === true) {
-  console.log("It's an array");
-} else {
-  console.log("It's not an array");
+// if (Array.isArray(x) === true) {
+//   console.log("It's an array");
+// } else {
+//   console.log("It's not an array");
+// }
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+// console.log("========gap=========");
+
+// //with loops
+// console.log("with loop");
+// let i = 10;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// //!======for loop=======
+// console.log("========for loop=========");
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// console.log("========do while loop=========");
+// let z = 0;
+// do {
+//   console.log(z);
+//   z++;
+// } while (z <= 0);
+
+let languages = ["Java", "nodeJS", "js", "ruby", "php", "python"];
+// console.log(languages[0]);
+// console.log(languages[1]);
+// console.log(languages[2]);
+// console.log(languages[3]);
+// console.log(languages[4]);
+// console.log(languages[5]);
+
+// console.log("=======with loop=====");
+// for (let i = 0; i <= languages.length - 1; i++) {
+//   console.log(languages[i]);
+// }
+// let xyz = " Java, nodeJS, js, ruby, php, python";
+// console.log(typeof xyz);
+// for (let i = 0; i <= xyz.length - 1; i++) {
+//   console.log(xyz[i]);
+// }
+// // ! ============== string immutable to mutable ===============
+// // ? convert string into array
+// // ? let we use split
+// let langArray = xyz.split(",");
+// console.log(langArray);
+// for (let i = 0; i <= langArray.length - 1; i++) {
+//   console.log(langArray[i]);
+// }
+let x = ["Java", "nodeJS", "js"];
+let y = ["ruby", "php", "python"];
+let z = x.concat(y);
+console.log(z);
+function lang(...rest) {
+  return rest;
 }
-//
+console.log(lang("Java", "nodeJS", "js", "ruby", "php", "python"));
